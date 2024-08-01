5692201f82cca2d6d0ac3811e72788cff58ef0c5
Hesus <74011780+unclehesus@users.noreply.github.com>
2024-08-01 12:10:24 +0200

    Update client firefighter.js

--- client firefighter.js
+++ client firefighter.js
@@ -1,8 +1,7 @@
 mp.events.add('fireCreated', (x, y, z) => {
     mp.game.graphics.notify("~r~Пожар начался! Координаты: ~w~X: " + x + ", Y: " + y + ", Z: " + z);
-    // Можно добавить маркер на карте
     const blip = mp.blips.new(436, new mp.Vector3(x, y, z), { name: "Пожар", color: 1, shortRange: false });
-    setTimeout(() => blip.destroy(), 600000); // Удаляем маркер через 10 минут
+    setTimeout(() => blip.destroy(), 600000);
 });
 
 mp.events.add('fireExtinguished', (x, y, z) => {
